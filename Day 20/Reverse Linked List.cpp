735c0dc733ed19b2056993e2262bdde85804e0b9
Sangam choudhary <Sangamchoudhary7@gmail.com>
2022-04-12 12:54:19 +0530

    day 20

---
+++ Day 20/Reverse Linked List.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        while(head){
+            ListNode* temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+};
