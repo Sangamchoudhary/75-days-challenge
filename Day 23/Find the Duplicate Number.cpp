941cc7ff0bd23236398f1c3c26a3691ad7337064
Sangam choudhary <Sangamchoudhary7@gmail.com>
2022-04-15 15:13:06 +0530

    day 23

---
+++ Day 23/Find the Duplicate Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr){
+        //piegeonhole principle
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[abs(arr[i])] < 0) return abs(arr[i]);
+            else arr[abs(arr[i])] *= -1; 
+        }
+        return -1;
+        
+        //normal sum
+        // int sum = 0;
+        // for(auto it : arr) sum += it;
+        // int totalNumber = n-1;
+        // int orgSum = (totalNumber * (totalNumber + 1)) / 2;
+        // int ans = sum - orgSum;
+        // return ans;
+    }
+};
